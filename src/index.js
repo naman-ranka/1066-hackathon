016af8a00883dd76fdda0952cd665f3bea574189
Naman Ranka <namanranka14@gmail.com>
2025-02-25 13:09:05 -0700

    tasks page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import routing components
+import App from './App'; // Your main app component (if you have one)
+import TaskList from './components/taskList'; // Your TaskList component
+import './index.css'; // Your global styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router> {/* Wrap your entire app with Router */}
+      <Routes> {/* Define your routes */}
+        <Route path="/" element={<App />} /> {/* Default route (optional) */}
+        <Route path="/tasks" element={<TaskList />} /> {/* Route for your tasks page */}
+        {/* Add other routes here, e.g., for your bill splitting components */}
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
